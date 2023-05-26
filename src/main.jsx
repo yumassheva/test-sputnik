99bac6d6785d44a7f3773382d8e3b7f8185701b6
yumassheva <mariia.yumasheva@yandex.ru>
2023-05-26 14:07:51 +0300

    four-commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,26 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import "./assets/fonts/Outfit-Regular.ttf";
-import { RouterProvider,  HashRouter } from "react-router-dom";
+import {  HashRouter } from "react-router-dom";
 
-const router = HashRouter(
-  [
-    {
-      path: "/portfolio",
-      element: [],
-    },
-    {
-      path: "/team",
-      element: [],
-    },
-    {
-      path: "/",
-      element: <App />,
-    },
-  ],
-);
 ReactDOM.createRoot(document.getElementById("root")).render(
-   <React.StrictMode>
-    <RouterProvider router={router} />
-    </React.StrictMode>
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 );
